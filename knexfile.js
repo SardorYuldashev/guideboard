fe666d88d7c2416d523d14ee5e9f652c23beb3b8
Sardor <henkook-baron@mail.ru>
2023-08-29 18:09:17 +0500

    install knex and pg

---
+++ knexfile.js
@@ -0,0 +1,72 @@
+const config = require('./src/shared/config');
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: config.db.host,
+      port: config.db.port,
+      database: config.db.name,
+      user: config.db.user,
+      password: config.db.password,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: 'db/migrations',
+    },
+    seeds: {
+      directory: 'db/seeds',
+    },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      host: config.db.host,
+      port: config.db.port,
+      database: config.db.name,
+      user: config.db.user,
+      password: config.db.password,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: 'db/migrations',
+    },
+    seeds: {
+      directory: 'db/seeds',
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      host: config.db.host,
+      port: config.db.port,
+      database: config.db.name,
+      user: config.db.user,
+      password: config.db.password,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: 'db/migrations',
+    },
+    seeds: {
+      directory: 'db/seeds',
+    },
+  },
+};
