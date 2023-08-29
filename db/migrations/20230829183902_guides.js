4f7094301828aa774e27a8debcf13a7ee2205225
Sardor <henkook-baron@mail.ru>
2023-08-29 23:49:00 +0500

    create guides migration and seed

---
+++ db/migrations/20230829183902_guides.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('guides', (table) => {
+    table.increments('id');
+    table.string('title', 200).notNullable();
+    table.string('content', 1000).notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('guides');
+};
