ea83be613f678e2a33aa182ace876721c27a008a
Sardor <henkook-baron@mail.ru>
2023-08-31 00:00:24 +0500

    add JOIN in showUserGuide service

--- src/modules/user_guide/show-user-guide.js
+++ src/modules/user_guide/show-user-guide.js
@@ -3,15 +3,38 @@ const { NotFoundError, ForbiddenError } = require('../../shared/errors');
 
 const showUserGuide = async ({ id, user }) => {
   const userGuide = await db('user_guide')
-    .where({ id })
-    .select()
+    .leftJoin('guides', 'guides.id', 'user_guide.guide_id')
+    .leftJoin('users', 'users.id', 'user_guide.user_id')
+    .where({ 'user_guide.id': id })
+    .select(
+      'user_guide.id',
+      'user_guide.completed',
+      db.raw(`
+        CASE WHEN user_guide.guide_id IS NULL THEN NULL
+        ELSE json_build_object(
+          'id', guides.id,
+          'title', guides.title,
+          'content', guides.content
+        ) END as guide
+      `),
+      db.raw(`
+        CASE WHEN user_guide.user_id IS NULL THEN NULL
+        ELSE json_build_object(
+          'id', users.id,
+          'first_name', users.first_name,
+          'last_name', users.last_name,
+          'username', users.username
+        ) END as user
+      `),
+    )
+    .groupBy('user_guide.id', 'guides.id', 'users.id')
     .first();
 
   if (!userGuide) {
     throw new NotFoundError("Foydalanuvchi uchun qo'llanma topilmadi");
   };
 
-  if (userGuide.user_id !== user.id) {
+  if (userGuide.user.id !== user.id) {
     throw new ForbiddenError("Boshqalarni qo'llanmasini ko'rish mumkin emas");
   };
 
@@ -19,3 +42,18 @@ const showUserGuide = async ({ id, user }) => {
 };
 
 module.exports = showUserGuide;
+//   const userGuide = await db('user_guide')
+//     .where({ id })
+//     .select()
+//     .first();
+
+//   if (!userGuide) {
+//     throw new NotFoundError("Foydalanuvchi uchun qo'llanma topilmadi");
+//   };
+
+//   if (userGuide.user_id !== user.id) {
+//     throw new ForbiddenError("Boshqalarni qo'llanmasini ko'rish mumkin emas");
+//   };
+
+//   return userGuide;
+// };
