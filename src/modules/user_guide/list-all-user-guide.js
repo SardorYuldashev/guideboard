3a8427cd0e5d0a8faf95a40339bad566cf9fdc06
Sardor <henkook-baron@mail.ru>
2023-08-31 00:21:07 +0500

    add JOIN in listUserGuide and listAllUserGuide

--- src/modules/user_guide/list-all-user-guide.js
+++ src/modules/user_guide/list-all-user-guide.js
@@ -2,8 +2,31 @@ const db = require('../../db');
 
 const listAllUserGuide = async ({ page = { limit: 10, offset: 0 }, filters = { completed: false } }) => {
   const dbQuery = db('user_guide')
-    .select()
-    .where(filters);
+    .leftJoin('guides', 'guides.id', 'user_guide.guide_id')
+    .leftJoin('users', 'users.id', 'user_guide.user_id')
+    .select(
+      'user_guide.id',
+      'user_guide.completed',
+      db.raw(`
+        CASE WHEN user_guide.guide_id IS NULL THEN NULL
+        ELSE json_build_object(
+          'id', guides.id,
+          'title', guides.title,
+          'content', guides.content
+        ) END as guide
+      `),
+      db.raw(`
+        CASE WHEN user_guide.user_id IS NULL THEN NULL
+        ELSE json_build_object(
+          'id', users.id,
+          'first_name', users.first_name,
+          'last_name', users.last_name,
+          'username', users.username
+        ) END as user
+      `),
+    )
+    .where(filters)
+    .groupBy('user_guide.id', 'guides.id', 'users.id');
 
   dbQuery.limit(page.limit).offset(page.offset);
 
