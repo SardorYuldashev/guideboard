f0e4dcfe6be211745eee65dee472f2562c7ba1a1
Sardor <henkook-baron@mail.ru>
2023-09-09 13:24:17 +0500

    experient. edit cors optional

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // experiment boshlandi
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://guide-board.netlify.app/',
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
