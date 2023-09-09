39a82c85de73e57dacb654fb3fb43b228ce30c7d
Sardor <henkook-baron@mail.ru>
2023-09-09 21:36:21 +0500

    add host in src/db/index.js

--- src/db/index.js
+++ src/db/index.js
@@ -7,6 +7,7 @@ const knex = require('knex');
 const db = knex({
   client: 'postgresql',
   connection: {
+    host: config.db.host,
     database: config.db.name,
     user: config.db.user,
     password: config.db.password,
